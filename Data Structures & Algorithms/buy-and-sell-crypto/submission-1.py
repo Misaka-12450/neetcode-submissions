1ae9dfab6a51eb3648ec97b7cd1627fa98b8e6ce
Misaka-12450 <8287084+Misaka-12450@users.noreply.github.com>
2026-04-24 17:10:58 +1000

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        min_price = prices[0]
+
+        for day in prices:
+            if day < min_price:
+                min_price = day
+            elif (profit := day - min_price) > max_profit:
+                max_profit = profit
+            
+        return max_profit
+        
